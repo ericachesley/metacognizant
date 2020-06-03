1b0a72068bcb454827bf5c813bf725fca1efcc2c
Erica Chesley <erchesley@gmail.com>
2020-06-03 14:14:54 -0700

    Set up index.html

--- server.py
+++ server.py
@@ -1,4 +1,16 @@
-from flask import Flask 
+from flask import Flask, render_template
+from model import connect_to_db
+
 
 app = Flask(__name__)
 app.secret_key = 'mygreatsecretkey'
+
+
+@app.route('/')
+def show_app():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
